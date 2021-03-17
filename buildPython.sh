4c72c0e5149ba993f42897d17901ceb9158d8637
manhnh251995 <41736966+manhnh251995@users.noreply.github.com>
2021-03-17 16:43:38 +0700

    Update buildPython.sh

--- buildPython.sh
+++ buildPython.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t pythonfinal:latest -f  DockerfilePython .
-docker tag pythonfinal:latest manhnh1995/pythonfinal:latest
+docker build -t pythonfinal:$VERSION -f  DockerfilePython .
+docker tag pythonfinal:latest manhnh1995/pythonfinal:$VERSION
 docker login -u manhnh1995 -p $PASSWORD_DOCKER_HUB
-docker push manhnh1995/nodejsfinal:$VERSION
+docker push manhnh1995/pythonfinal:$VERSION
