27426a7a3f4c04934a9ab9ae003d5b9397df3006
manhnh251995 <41736966+manhnh251995@users.noreply.github.com>
2021-03-18 09:29:49 +0700

    Create updateNodeJS.sh

---
+++ updateNodeJS.sh
@@ -0,0 +1,6 @@
+!/bin/bash
+#stop container and remove
+docker rm --force nodejs-frontend
+docker login -u manhnh1995 -p $PASSWORD_DOCKER_HUB
+docker pull manhnh1995/nodejsfinal:latest 
+docker run --name nodejs-frontend -d -p 3000:3000 -e VERSION=latest -e HOSTNAME=$(hostname) nodejsfinal:latest
