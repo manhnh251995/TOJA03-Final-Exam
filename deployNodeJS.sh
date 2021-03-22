7e469129af522b44647998da8e71035c1d8a482e
manhnh251995 <41736966+manhnh251995@users.noreply.github.com>
2021-03-22 20:27:25 +0700

    Update deployNodeJS.sh

--- deployNodeJS.sh
+++ deployNodeJS.sh
@@ -1,3 +1,3 @@
 docker login -u manhnh1995 -p $PASSWORD_DOCKER_HUB
 docker pull manhnh1995/nodejsfinal:$VERSION
-docker run -d -p 3000:3000 -e VERSION=$VERSION -e HOSTNAME=$(hostname) nodejsfinal:$VERSION
+docker run -d -p 3000:3000 -e VERSION=$VERSION -e HOSTNAME=$(hostname) manhnh1995/nodejsfinal:$VERSION
